bd578c0c6cf020f740008d652c96bc99cdd987dd
Intuch.bu <intuch.bu@gmail.com>
2026-06-24 10:59:30 +0700

    feat: enhance ProductCard and ProductHighlight components with dynamic props

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -13,9 +13,9 @@ function ProductHighlight() {
             <ProductCard imgSrc="https://picsum.photos/id/231/200/200" />
             
         */}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard productName="The Lorem Ipsum for photos." price="$99.99" imgSrc="https://picsum.photos/id/231/200/200" />
+          <ProductCard productName="Wireless Bluetooth Earbuds" price="$79.99" imgSrc="https://picsum.photos/id/232/200/200" />
+          <ProductCard productName="Stylish Cool Sunglasses in The World" price="$79.99" imgSrc="https://picsum.photos/id/200/200" />
         </div>
       </div>
     </section>
